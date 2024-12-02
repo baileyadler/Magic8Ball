02a1ed4b712cfe5aaa8c3e26ac532e0b3d66543a
baileyadler <526BAdler@frhsd.com>
2024-12-02 11:55:00 -0500

    added images that WORK and black background

--- script.js
+++ script.js
@@ -6,36 +6,46 @@ userName
 function shakeMagic8Ball() {
   let randomNumber = Math.floor(Math.random() * 8);
   let eightBall = "";
+  let responseImg;
 
   let userQuestion = prompt("what is your question for the mighty 8 Ball?");
   switch (randomNumber) {
     case 0:
       eightBall = "It is certain";
+      responseImg = "itiscertain.jpg";
       break;
     case 1:
       eightBall = "It is decidedly so";
+      responseImg = "itissaidso.webp";
       break;
     case 2:
       eightBall = "Reply hazy try again";
+      responseImg = "replyhazy.png";
       break;
     case 3:
       eightBall = "Cannot predict now";
+      responseImg = "dontpredict.webp";
       break;
     case 4:
       eightBall = "Do not count on it";
+      responseImg = "donotcountonit.jpg";
       break;
     case 5:
       eightBall = "My sources say no";
+      responseImg = "sourxessayno.png";
       break;
     case 6:
       eightBall = "Outlook not so good";
+      responseImg = "outlooknotsogood.png";
       break;
     case 7:
       eightBall = "Signs point to yes";
+      responseImg = "signpointstoyes.jpg";
       break;
   }
   console.log(eightBall);
   document.getElementById(
     "response-text"
   ).innerText = `${userName} has asked the Magic 8 Ball ${userQuestion}. The wise and wondrous has replied ${eightBall}`;
+  document.getElementById("response-image").src = responseImg;
 }
